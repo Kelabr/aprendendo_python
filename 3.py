e5d848ae6979d94f1d29ce89078068d36aea16e5
Kelabr <kelabrantes.me@gmail.com>
2025-04-28 15:51:37 -0300

    add comentátio

--- 3.py
+++ 3.py
@@ -1,3 +1,8 @@
+# Desafio: Palavras Palíndromas
+# Objetivo: Escrever uma função em Python que receba uma lista de palavras e retorne uma nova lista contendo apenas as palavras que são palíndromas.
+
+# Uma palavra é considerada palíndroma se ela for igual à sua inversão. Por exemplo, "radar", "level" e "madam" são palíndromas.
+
 words = ['radar', 'python', 'level', 'world', 'madam']
 
 def palindroma_function(list):
